9982e51a9f8dd9313b4e05c699777976f31c0b8f
Kyle Mendell <kmendell@ofkm.us>
2025-04-21 19:42:02 -0500

    only show actions based on state

--- src/routes/containers/ContainerActions.svelte
+++ src/routes/containers/ContainerActions.svelte
@@ -5,11 +5,14 @@
   import { goto } from "$app/navigation";
   import { Eye, Play, RotateCcw, StopCircle, Trash2 } from "@lucide/svelte";
 
-  let { id }: { id: string } = $props();
+  let { id, state }: { id: string; state: string } = $props();
 
   function viewContainer() {
     goto(`/containers/${id}`);
   }
+
+  // Determine if container is running
+  const isRunning = $derived(state === "running");
 </script>
 
 <DropdownMenu.Root>
@@ -30,27 +33,36 @@
     <DropdownMenu.Group>
       <DropdownMenu.Item onclick={() => viewContainer()}>
         <Eye class="w-4 h-4" />
-        View</DropdownMenu.Item
-      >
-      <DropdownMenu.Item onclick={() => viewContainer()}>
-        <Play class="w-4 h-4" />
-        Start</DropdownMenu.Item
-      >
-      <DropdownMenu.Item onclick={() => viewContainer()}>
-        <RotateCcw class="w-4 h-4" />
-        Restart</DropdownMenu.Item
-      >
-      <DropdownMenu.Item onclick={() => viewContainer()}>
-        <StopCircle class="w-4 h-4" />
-        Stop</DropdownMenu.Item
-      >
+        View
+      </DropdownMenu.Item>
+
+      {#if !isRunning}
+        <!-- Show Start action only if container is not running -->
+        <DropdownMenu.Item onclick={() => viewContainer()}>
+          <Play class="w-4 h-4" />
+          Start
+        </DropdownMenu.Item>
+      {:else}
+        <!-- Show Restart and Stop actions only if container is running -->
+        <DropdownMenu.Item onclick={() => viewContainer()}>
+          <RotateCcw class="w-4 h-4" />
+          Restart
+        </DropdownMenu.Item>
+
+        <DropdownMenu.Item onclick={() => viewContainer()}>
+          <StopCircle class="w-4 h-4" />
+          Stop
+        </DropdownMenu.Item>
+      {/if}
+
+      <!-- Always show Remove action -->
       <DropdownMenu.Item
         class="text-red-500 focus:!text-red-700"
         onclick={() => viewContainer()}
       >
         <Trash2 class="w-4 h-4" />
-        Remove</DropdownMenu.Item
-      >
+        Remove
+      </DropdownMenu.Item>
     </DropdownMenu.Group>
   </DropdownMenu.Content>
 </DropdownMenu.Root>
