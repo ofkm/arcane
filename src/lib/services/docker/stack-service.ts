10ccd5562ccd4fef0e3b979d836848a72cb027ca
Kyle Mendell <kmendell@ofkm.us>
2025-04-26 01:37:57 -0500

    chore: use node:fs and node:path

--- src/lib/services/docker/stack-service.ts
+++ src/lib/services/docker/stack-service.ts
@@ -1,5 +1,5 @@
-import { promises as fs } from 'fs';
-import { join } from 'path';
+import { promises as fs } from 'node:fs';
+import { join } from 'node:path';
 import DockerodeCompose from 'dockerode-compose';
 import yaml from 'js-yaml';
 import { nanoid } from 'nanoid';
