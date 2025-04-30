6c5cc79604ea518da5611d74d0af05358ca2efc2
Kyle Mendell <ksm@ofkm.us>
2025-04-29 20:14:44 -0500

    fix app.d.ts

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,32 +1,25 @@
 import type { VolumeInspectInfo as OriginalVolumeInspectInfo } from 'dockerode';
+import type { User } from '$lib/types/user.type';
 
-// See https://svelte.dev/docs/kit/types#app.d.ts
-// for information about these interfaces
 declare global {
 	namespace App {
 		// interface Error {}
-// src/app.d.ts
-import type { VolumeInspectInfo as OriginalVolumeInspectInfo } from 'dockerode';
-import type { User } from '$lib/types/user.type';
-
-// See https://svelte.dev/docs/kit/types#app.d.ts
-
-declare global {
-  namespace App {
-    interface Locals {
-      user?: User;
-      session?: {
-        id: string;
-        userId: string;
-        username: string;
-        created: string;
-        expires: string;
-        ip?: string;
-        userAgent?: string;
-      };
-    }
-  }
-}
+		declare global {
+			namespace App {
+				interface Locals {
+					user?: User;
+					session?: {
+						id: string;
+						userId: string;
+						username: string;
+						created: string;
+						expires: string;
+						ip?: string;
+						userAgent?: string;
+					};
+				}
+			}
+		}
 		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}
