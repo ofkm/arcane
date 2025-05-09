6c56215f950da790609c3d579c72d550608dde36
Kyle Mendell <ksm@ofkm.us>
2025-05-09 02:20:57 -0500

    fix: show error messages in toasts (container api routes)

--- src/lib/utils/api.util.ts
+++ src/lib/utils/api.util.ts
@@ -1,5 +1,6 @@
 import type { Result } from './try-catch';
 import { toast } from 'svelte-sonner';
+import { extractDockerErrorMessage } from '$lib/utils/errors.util';
 
 /**
  * Handles the result of an API call, showing errors or executing a success callback.
@@ -17,6 +18,21 @@ export function handleApiReponse<T>(result: Result<T, Error>, message: string, s
 		return;
 	} else if (result.data) {
 		onSuccess(result.data);
+		setLoadingState(false);
+	}
+}
+
+export function handleApiResultWithCallbacks<T>({ result, message, setLoadingState = () => {}, onSuccess, onError = () => {} }: { result: Result<T, Error>; message: string; setLoadingState?: (value: boolean) => void; onSuccess: (result: Result<T, Error>) => void; onError?: (result: Result<T, Error>) => void }) {
+	setLoadingState(true);
+	if (result.error) {
+		const dockerMsg = extractDockerErrorMessage(result.error);
+		console.error(`onErrorCallback: ${message}:`, result.error);
+		toast.error(`${message}: ${dockerMsg}`);
+		setLoadingState(false);
+		onError(result);
+		return;
+	} else if (result.data) {
+		onSuccess(result);
 		setLoadingState(false);
 	}
 }
