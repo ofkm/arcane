10a054eff890716d0d64aad44b45892d6460b9b7
Kyle Mendell <ksm@ofkm.us>
2025-06-19 21:35:16 -0500

    remove broken imports

--- frontend/src/lib/services/api/index.ts
+++ frontend/src/lib/services/api/index.ts
@@ -1,7 +1,6 @@
 import ContainerAPIService from './container-api-service';
 import ImageAPIService from './image-api-service';
 import ImageMaturityAPIService from './image-maturity-api-service';
-import VolumeAPIService from './volume-api-service';
 import StackAPIService from './stack-api-service';
 import SystemAPIService from './system-api-service';
 import TemplateAPIService from './template-api-service';
@@ -12,7 +11,6 @@ import TemplateRegistryAPIService from './template-registry-api-service';
 import OidcAPIService from './oidc-api-service';
 import EncryptionAPIService from './encryption-api-service';
 import DeploymentAPIService from './deployment-api-service';
-import ValidationAPIService from './validation-api-service';
 import AppConfigAPIService from './appconfig-api-service';
 import AgentAPIService from './agent-api-service';
 import AgentStackAPIService from './agent-stack-api-service';
@@ -25,7 +23,6 @@ import EnvironmentManagementAPIService from './environment-management-api-servic
 export const containerAPI = new ContainerAPIService();
 export const imageAPI = new ImageAPIService();
 export const imageMaturityAPI = new ImageMaturityAPIService();
-export const volumeAPI = new VolumeAPIService();
 export const stackAPI = new StackAPIService();
 export const systemAPI = new SystemAPIService();
 export const templateAPI = new TemplateAPIService();
@@ -36,7 +33,6 @@ export const templateRegistryAPI = new TemplateRegistryAPIService();
 export const oidcAPI = new OidcAPIService();
 export const encryptionAPI = new EncryptionAPIService();
 export const deploymentAPI = new DeploymentAPIService();
-export const validationAPI = new ValidationAPIService();
 export const appConfigAPI = new AppConfigAPIService();
 export const agentAPI = new AgentAPIService();
 export const agentStackAPI = new AgentStackAPIService();
@@ -50,7 +46,6 @@ interface APIServices {
 	container: ContainerAPIService;
 	image: ImageAPIService;
 	imageMaturity: ImageMaturityAPIService;
-	volume: VolumeAPIService;
 	stack: StackAPIService;
 	system: SystemAPIService;
 	template: TemplateAPIService;
@@ -61,7 +56,6 @@ interface APIServices {
 	oidc: OidcAPIService;
 	encryption: EncryptionAPIService;
 	deployment: DeploymentAPIService;
-	validation: ValidationAPIService;
 	appConfig: AppConfigAPIService;
 	agent: AgentAPIService;
 	converter: ConverterAPIService;
@@ -75,7 +69,6 @@ const apiServices: APIServices = {
 	container: containerAPI,
 	image: imageAPI,
 	imageMaturity: imageMaturityAPI,
-	volume: volumeAPI,
 	stack: stackAPI,
 	system: systemAPI,
 	template: templateAPI,
@@ -86,7 +79,6 @@ const apiServices: APIServices = {
 	oidc: oidcAPI,
 	encryption: encryptionAPI,
 	deployment: deploymentAPI,
-	validation: validationAPI,
 	appConfig: appConfigAPI,
 	agent: agentAPI,
 	converter: converterAPI,
