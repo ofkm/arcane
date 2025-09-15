9a6586a36bb6c8f456e5262b9a354c018c57108c
Kyle Mendell <ksm@ofkm.us>
2025-09-15 11:32:33 -0500

    centralize constants

--- backend/internal/utils/registry/constants.go
+++ backend/internal/utils/registry/constants.go
@@ -1,5 +1,6 @@
 package registry
 
 const DefaultRegistry = "registry-1.docker.io"
-
+const DefaultRegistryDomain = "docker.io"
+const DefaultRegistryHost = "index.docker.io"
 const ContentDigestHeader = "Docker-Content-Digest"
--- backend/internal/utils/registry/helpers.go
+++ backend/internal/utils/registry/helpers.go
@@ -1,10 +1,5 @@
 package registry
 
-const (
-	DefaultRegistryDomain = "docker.io"
-	DefaultRegistryHost   = "index.docker.io"
-)
-
 func GetRegistryAddress(imageRef string) (string, error) {
 	named, err := parseNormalizedNamed(imageRef)
 	if err != nil {
