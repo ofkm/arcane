17a92e741fb455d465e96465cb0ac1cb88beecb6
Kyle Mendell <ksm@ofkm.us>
2025-09-17 14:49:40 -0500

    fix postgres migration

--- backend/resources/migrations/postgres/014_containers_project_fk.down.sql
+++ backend/resources/migrations/postgres/014_containers_project_fk.down.sql
@@ -1,6 +1,6 @@
 BEGIN;
 -- Drop new FK
-ALTER TABLE IF EXISTS containers
+ALTER TABLE containers
   DROP CONSTRAINT IF EXISTS containers_project_id_fkey;
 
 -- Rename column back
@@ -10,7 +10,7 @@ ALTER TABLE containers
 -- Optionally restore FK to stacks if it exists
 DO $$
 BEGIN
-  IF EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name='stacks') THEN
+  IF EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name='stacks' AND table_schema = current_schema()) THEN
     ALTER TABLE containers
       ADD CONSTRAINT containers_stack_id_fkey
       FOREIGN KEY (stack_id) REFERENCES stacks(id) ON DELETE SET NULL;
--- backend/resources/migrations/postgres/014_containers_project_fk.up.sql
+++ backend/resources/migrations/postgres/014_containers_project_fk.up.sql
@@ -5,17 +5,18 @@ BEGIN
   SELECT tc.constraint_name INTO cname
   FROM information_schema.table_constraints tc
   JOIN information_schema.key_column_usage kcu ON tc.constraint_name = kcu.constraint_name AND tc.table_name = kcu.table_name
-  WHERE tc.table_name = 'containers' AND tc.constraint_type = 'FOREIGN KEY' AND kcu.column_name = 'stack_id';
+  WHERE tc.table_name = 'containers' AND tc.constraint_type = 'FOREIGN KEY' AND kcu.column_name = 'stack_id'
+    AND tc.table_schema = current_schema() AND kcu.table_schema = current_schema();
   IF cname IS NOT NULL THEN
     EXECUTE format('ALTER TABLE containers DROP CONSTRAINT %I', cname);
   END IF;
 END$$;
 
 ALTER TABLE containers
-  RENAME COLUMN IF EXISTS stack_id TO project_id;
+  RENAME COLUMN stack_id TO project_id;
 
--- Create FK to projects if table exists
-ALTER TABLE IF EXISTS containers
+-- Create FK to projects
+ALTER TABLE containers
   ADD CONSTRAINT containers_project_id_fkey
   FOREIGN KEY (project_id) REFERENCES projects(id) ON DELETE SET NULL;
 COMMIT;
