5e01ccd51efd600b8f6b37ac79e26c956e43fce5
cabaucom376 <cabaucom376@gmail.com>
2025-10-30 18:35:32 -0400

    lint

--- frontend/src/routes/projects/components/CodePanel.svelte
+++ frontend/src/routes/projects/components/CodePanel.svelte
@@ -23,14 +23,14 @@
 </script>
 
 <Card.Root class="flex flex-col lg:h-full">
-	<Card.Header icon={FileTextIcon} class="flex-shrink-0 items-center">
+	<Card.Header icon={FileTextIcon} class="shrink-0 items-center">
 		<Card.Title>
 			<h2>
 				{title}
 			</h2>
 		</Card.Title>
 	</Card.Header>
-	<Card.Content class="min-h-[500px] flex-grow p-0 lg:h-full">
+	<Card.Content class="min-h-[500px] grow p-0 lg:h-full">
 		<div class="h-full [&_.cm-content]:text-xs sm:[&_.cm-content]:text-sm">
 			<CodeEditor bind:value {language} {placeholder} />
 			{#if error}
